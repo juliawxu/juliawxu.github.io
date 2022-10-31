75a05caafe79d14e7792e69b6039a63778a91084
juliawxu <julia.xu.44@gmail.com>
2022-10-30 22:59:00 -0500

    Updates

---
+++ precache-manifest.94c48d6f93da126fd7157101770b69b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "26fb92d95d4ef02e5899",
+    "url": "/static/css/main.e8b42530.chunk.css"
+  },
+  {
+    "revision": "26fb92d95d4ef02e5899",
+    "url": "/static/js/main.26fb92d9.chunk.js"
+  },
+  {
+    "revision": "ae5b835116b0200f75a6",
+    "url": "/static/js/1.ae5b8351.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "700470459a2f1b3739b34a33b7af593d",
+    "url": "/static/media/linkedin-icon-96.70047045.svg"
+  },
+  {
+    "revision": "07b170ca96cdc6a9da56df0194d22f94",
+    "url": "/static/media/github-icon-90.07b170ca.svg"
+  },
+  {
+    "revision": "a162b4246e36e0721a855008bf49c8cc",
+    "url": "/static/media/portrait.a162b424.GIF"
+  },
+  {
+    "revision": "85744fb00b685fb37932160c020085d0",
+    "url": "/index.html"
+  }
+];
